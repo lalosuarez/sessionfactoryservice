54d0d8b38484c2aad4d288ad0a67b9fa8e891583
Lalo Suarez <lalos.suarez@gmail.com>
2014-10-28 15:19:02 -0600

    First commit.

---
+++ src/main/java/org/lalosuarez/hibernate/SessionFactoryService.java
@@ -0,0 +1,21 @@
+package org.lalosuarez.hibernate;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+public class SessionFactoryService {
+
+	private final SessionFactory sessionFactory;
+
+	public SessionFactoryService() {
+	    Configuration configuration = new Configuration().configure();
+	    ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+	    sessionFactory = configuration.buildSessionFactory(serviceRegistry);		
+	}
+
+	public SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+}
